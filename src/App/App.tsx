5b8cad36abeda64b431780d3ccdaf074ab5f5281
Przemek <ppadamczyk257@gmail.com>
2021-05-26 10:44:00 +0200

    cleanup

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,17 +4,16 @@ import * as P from './parts';
 import * as C from './constants';
 import { apiRequestHandler, getMarksToReplace } from './helpers';
 import { getCurrencies } from '../store/selectors';
+import { ErrorType } from '../useErrorHandler/constants';
+import useErrorHandler from '../useErrorHandler/useErrorHandler';
 import { addCurrencyToStore, updateCurrencyPriceInfo } from '../store/actions';
 import { ApplicationState, CurrenciesState, Currency } from '../store/constants';
-import useErrorHandler from '../useErrorHandler/useErrorHandler';
-import { ErrorType } from '../useErrorHandler/constants';
 
 const App: React.FC = () => {
    const [inputContent, setInputContent] = useState<string>('');
    const [outputContent, setOutputContent] = useState<string>('');
+   const symbolsMarkedAsIncorrect = useRef<string[]>([]);
    const currenciesInStore = useSelector<ApplicationState, CurrenciesState | undefined>(getCurrencies);
-
-   const incorrectSymbols = useRef<string[]>([]);
    
    const dispatch = useDispatch();
 
@@ -27,23 +26,20 @@ const App: React.FC = () => {
 
    const getCurrencyFromStoreOrAPI = async (symbol: string) => {
       // If given symbol is already marked as incorrect handle it as error skipping API request
-      if (incorrectSymbols.current.includes(symbol)) {
+      if (symbolsMarkedAsIncorrect.current.includes(symbol)) {
          return handleIncorrectSymbol(symbol);
       }
 
       return currenciesInStore?.[symbol]
          ? currenciesInStore[symbol]
          : apiRequestHandler(`https://api.coinpaprika.com/v1/search?q=${symbol}&c=currencies&modifier=symbol_search&limit=1`)
-            .catch((error) => addErrorMessage({
-               errorType: ErrorType.ApiResponseError,
-               errorMessage: error.error,
-            }))
+            .catch((error) => addErrorMessage({ errorType: ErrorType.ApiResponseError, errorMessage: error.error }))
             .then((responseAsJSON) => {
                if (responseAsJSON?.currencies?.[0]) {
                   dispatch(addCurrencyToStore(responseAsJSON.currencies[0]));
                   return responseAsJSON.currencies[0];
                } else {
-                  incorrectSymbols.current.push(symbol);
+                  symbolsMarkedAsIncorrect.current.push(symbol);
                   return handleIncorrectSymbol(symbol);
                }
             });
@@ -51,15 +47,11 @@ const App: React.FC = () => {
 
    const getPriceInUSDFromStoreOrAPI = async (currency: Currency) => currenciesInStore?.[currency.symbol]?.price
       ? currenciesInStore[currency.symbol]
-      : apiRequestHandler(`https://api.coinpaprika.com/v1/price-converter?base_currency_id=${currency.id}&quote_currency_id=usd-us-dollars&amount=1`)
-         .catch((error) => addErrorMessage({
-            errorType: ErrorType.ApiResponseError,
-            errorMessage: error.error,
-         }))   
+      : apiRequestHandler(`https://api.coinpaprika.com/v1/price-converter?base_currency_id=${currency.id}&quote_currency_id=usd-us-dollars&amount=1`).catch((error) => addErrorMessage({ errorType: ErrorType.ApiResponseError, errorMessage: error.error }))   
          .then((responseAsJSON) => {
-               dispatch(updateCurrencyPriceInfo(currency.symbol, responseAsJSON));
-               return responseAsJSON;
-            });
+            dispatch(updateCurrencyPriceInfo(currency.symbol, responseAsJSON));
+            return responseAsJSON;
+         });
 
    const establishOutputContent = (inputContent: string) => {
       let transformedContent = inputContent;
@@ -89,7 +81,7 @@ const App: React.FC = () => {
             }
             default:
                addErrorMessage({ errorType: ErrorType.IncorrectFunction, errorMessage: replacementFunction });
-               setOutputContent(transformedContent = transformedContent.replace(mark, 'INCORRECT_FUNCTION_NAME'));
+               setOutputContent(transformedContent = transformedContent.replace(mark, C.incorrectFunctionName));
          }
       })
    }
--- src/App/constants.ts
+++ src/App/constants.ts
@@ -2,3 +2,5 @@ export enum SupportedFunctions {
    Name = 'NAME',
    Price = 'PRICE',
 }
+
+export const incorrectFunctionName = 'INCORRECT_FUNCTION_NAME';
